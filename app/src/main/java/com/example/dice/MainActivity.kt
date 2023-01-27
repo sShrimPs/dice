d8e504e3539da901304999359c63a381914c0aa5
sShrimPs <yma1591@gmail.com>
2023-01-27 15:47:49 +0900

    naver-map

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -8,4 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun main(){
+        // 서버 역할을 하기 위해 객체 생성
+        val server = java.net.ServerSocket(55555)
+        print("사용자 접속대기")
+        val socket = server.accept()
+        println(socket)
+
+        // 서버 종료
+        socket.close()
+    }
 }
